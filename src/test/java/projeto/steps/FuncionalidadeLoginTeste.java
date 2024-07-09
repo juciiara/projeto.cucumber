ca6faf70fa316a00a5987ea4a2008f13899bbb2b
juciiara <juciarae.c@gmail.com>
2024-07-09 14:56:34 -0300

    projeto_cucumber

---
+++ src/test/java/projeto/steps/FuncionalidadeLoginTeste.java
@@ -0,0 +1,23 @@
+package projeto.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FuncionalidadeLoginTeste {
+
+	@Given("que esteja natela de login")
+	public void que_esteja_natela_de_login() {
+
+	}
+
+	@When("enviar os dados de login")
+	public void enviar_os_dados_de_login() {
+	}
+
+	@Then("login realizado com sucesso")
+	public void login_realizado_com_sucesso() {
+
+	}
+
+}
---
+++ target/test-classes/features/funcionalidade.login.feature
@@ -0,0 +1,12 @@
+#Author: juciarae.c@gmail.com
+ 
+Feature: Realizar login no site saucedemo
+Como usuarío do site saucedemo
+Quiero informar os dados de acesso 
+Para acessar a conta
+
+Scenario:Login valido  
+Given que esteja natela de login
+When enviar os dados de login
+Then login realizado com sucesso
+
